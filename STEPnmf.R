47ea4f3dcf395f49336ff6700204f955355618ae
dsqlucia <50531357+dsqlucia@users.noreply.github.com>
2024-04-30 12:33:15 +0200

    Update STEPnmf.R
    # 12272023: cz for testing nmf
    # 04262024: dsqlucia change the data input file (remove duplicate subjects)
    # 04272024: cz add nruns as nmf_analysis's input; add function nmf_consensus_map
    # 04282024: cz add the use of doParallel; not sure why the default parallel 'vP+nums'and BiocParallel package did not work after 1 year:(

--- STEPnmf.R
+++ STEPnmf.R
@@ -10,16 +10,18 @@ if (!requireNamespace('pacman', quietly = TRUE)) {
       install.packages('pacman')
 }
 
-curWD <- dirname(rstudioapi::getSourceEditorContext()$path)
-setwd(curWD)
 
 # pay attention to the message from biocManager, it will ask you to update the packages or not.
-if (!requireNamespace('Biobase', quietly = TRUE)) {
-      BiocManager::install("Biobase", force = TRUE)
-}
+# if (!require("BiocManager", quietly = TRUE))
+#   install.packages("BiocManager")
+
+#   BiocManager::install("BiocParallel")
+#   BiocManager::install("NMF")
+#   BiocManager::install("Biobase")
+
 
 # Load the necessary package
-pacman::p_load(BiocManager, Biobase, NMF)
+pacman::p_load(BiocManager, Biobase, NMF，doParallel，foreach)
 
 # Load data & filter useful columns
 load_data <- function(filename) {
@@ -33,18 +35,39 @@ load_data <- function(filename) {
   return(fad[, fadnames])
 }
 
-# need to check the file names
-CHN_Clean <- load_data(here::here("3_2_3_Save_points","CHN_Cleaned.csv"))
-ENG_Clean <- load_data(here::here("3_2_3_Save_points", "ENG_Cleaned.csv"))
-FRN_Clean <- load_data(here::here("3_2_3_Save_points", "FRN_Cleaned.csv"))
-JPN_1_Clean <- load_data(here::here("3_2_3_Save_points", "JPN_1_Cleaned.csv"))
-JPN_2_Clean <- load_data(here::here("3_2_3_Save_points", "JPN_2_Cleaned.csv"))
+
+CHN_Clean <- load_data(here::here("3_2_3_Save_points","CHN_230711.csv"))
+ENG_Clean <- load_data(here::here("3_2_3_Save_points", "ENG.csv"))
+FRN_Clean <- load_data(here::here("3_2_3_Save_points", "FRN.csv"))
+JPN_1_Clean <- load_data(here::here("3_2_3_Save_points", "JPN_1.csv"))
+JPN_2_Clean <- load_data(here::here("3_2_3_Save_points", "JPN_2.csv"))
+
 
 # Function to perform NMF Analysis
-nmf_analysis <- function(data, data_name, title_prefix) {
+nmf_analysis <- function(data, data_name, title_prefix, nruns) {
   data_t <- t(data)
   
-  res_data <- NMF::nmf(data_t, 4, nrun=500, seed=777, .opt="vP31")
+  # I am using windows with 14900k but only with 96GB memory, 
+  # so I am not going to use all cores
+  numCores <- parallel::detectCores() - 2  # Use all cores except two
+  cl <- makeCluster(numCores)
+  registerDoParallel(cl)
+  cat("Using", numCores, "cores for parallel computation\n")
+  
+  
+  options_string <- paste("p", numCores, sep="")  
+  
+  # if you are using macOS
+  # register(MulticoreParam(workers = parallel::detectCores() - 2))
+  # I am using windows with 14900k, so I cll SnowParam()
+  # bp_param <- SnowParam(workers = parallel::detectCores() - 2, type = "SOCK")
+  # register(bp_param)
+
+  res_data <- NMF::nmf(data_t, 
+                       4, 
+                       nrun=nruns, 
+                       seed=777)
+  
   #pdf(paste0(title_prefix, "_", data_name, "_4_components.pdf"))
   basismap(res_data, main = paste('4 components of', title_prefix))
   curr_plot <- recordPlot()
@@ -52,7 +75,12 @@ nmf_analysis <- function(data, data_name, title_prefix) {
   replayPlot(curr_plot)
   dev.off()
   
-  estim_data <- NMF::nmf(data_t, 2:9, nrun=500, seed=777, .opt="vP31")
+  estim_data <- NMF::nmf(data_t, 
+                         2:9, 
+                         nrun=nruns, 
+                         seed=777)
+  
+  
   plot(estim_data, main = paste('NMF estim of', title_prefix))
   curr_plot <- recordPlot()
   pdf(paste0(title_prefix, "_", data_name, "_estim.pdf"))
@@ -64,26 +92,68 @@ nmf_analysis <- function(data, data_name, title_prefix) {
   k.best <- which.max(coph_diff) + 1
   
   
-  res_data_best <- NMF::nmf(data_t, k.best, seed=9527)
+  res_data_best <- NMF::nmf(data_t, 
+                            k.best, 
+                            nrun=nruns, 
+                            seed=777) 
+  
+  
   basismap(res_data_best, main = paste('Best NMF is ',k.best,' of',title_prefix))
   curr_plot <- recordPlot()
   pdf(paste0(title_prefix, "_", data_name, "_best_NMF.pdf"))
   replayPlot(curr_plot)
   dev.off()
+  
+  
+  stopCluster(cl)
 }
 
+nruns <- 200
+nmf_analysis(CHN_Clean, "CHN_Clean", "FADplus_NMF", nruns)
+nmf_analysis(ENG_Clean, "ENG_Clean", "FADplus_NMF", nruns)
+nmf_analysis(FRN_Clean, "FRN_Clean", "FADplus_NMF", nruns)
+nmf_analysis(JPN_1_Clean, "JPN_1_Clean", "FADplus_NMF", nruns)
+nmf_analysis(JPN_2_Clean, "JPN_2_Clean", "FADplus_NMF", nruns)
 
-nmf_analysis(CHN_Clean, "CHN_Clean", "FADplus_NMF")
-nmf_analysis(ENG_Clean, "ENG_Clean", "FADplus_NMF")
-nmf_analysis(FRN_Clean, "FRN_Clean", "FADplus_NMF")
-nmf_analysis(JPN_1_Clean, "JPN_1_Clean", "FADplus_NMF")
-nmf_analysis(JPN_2_Clean, "JPN_2_Clean", "FADplus_NMF")
 
 
-## test
-consensusmap(estim_data, annCol=data_t, labCol=NA, labRow=NA)
+## consensus_map
+nmf_consensus_map <- function(data, data_name, title_prefix, rank_range, nruns) {
+  data_t <- t(data)
+  
+  numCores <- parallel::detectCores() - 2  # Use all cores except two
+  cl <- makeCluster(numCores)
+  registerDoParallel(cl)
+  cat("Using", numCores, "cores for parallel computation\n")
+  
+  nmf_fit <- NMF::nmf(data_t, 
+                      rank=rank_range, 
+                      nrun=nruns, 
+                      seed=777) 
+  
+  pdf(paste0(title_prefix, "_", data_name, "_consensus_map.pdf"))
+  consensusmap(nmf_fit, main=paste0('Consensus Map for ', data_name))
+  dev.off()
+  
+  stopCluster(cl)
+  
+}
+rank_range <- 2:9
+nmf_consensus_map(CHN_Clean, "CHN_Clean", "FADplus_NMF", rank_range, nruns)
+nmf_consensus_map(ENG_Clean, "ENG_Clean", "FADplus_NMF", rank_range, nruns)
+nmf_consensus_map(FRN_Clean, "FRN_Clean", "FADplus_NMF", rank_range, nruns)
+nmf_consensus_map(JPN_1_Clean, "JPN_1_Clean", "FADplus_NMF", rank_range, nruns)
+nmf_consensus_map(JPN_2_Clean, "JPN_2_Clean", "FADplus_NMF", rank_range, nruns)
 
-##
+
+
+
+
+
+
+## no use ##
+## no use ##
+## no use ##
 Half_Clean <- load_data(here::here("3_2_3_Save_points","halftestnmf.csv"))
 
 ##
@@ -94,17 +164,15 @@ data_t <- t(data)
 title_prefix <- "FADplus_NMF"
 data_name <- "Half_Clean"
 
-res_data <- NMF::nmf(data_t, 4, nrun=500, seed=9527, .opt="vP31")
-
+res_data <- NMF::nmf(data_t, 4, nrun=1000, seed=777, .opt="vP31")
 #pdf(paste0(title_prefix, "_", data_name, "_4_components.pdf"))
 basismap(res_data, main = paste('4 components of', title_prefix))
-
 curr_plot <- recordPlot()
 pdf(paste0(title_prefix, "_", data_name, "_4_components.pdf"))
 replayPlot(curr_plot)
 dev.off()
 
-estim_data <- NMF::nmf(data_t, 2:9, nrun=500, seed=9527, .opt="vP31")
+estim_data <- NMF::nmf(data_t, 2:9, nrun=1000, seed=777, .opt="vP31")
 plot(estim_data, main = paste('NMF estim of', title_prefix))
 curr_plot <- recordPlot()
 pdf(paste0(title_prefix, "_", data_name, "_estim.pdf"))
@@ -116,7 +184,7 @@ coph_diff <- c(diff(coph))
 k.best <- which.max(coph_diff) + 1
 
 
-res_data_best <- NMF::nmf(data_t, k.best,nrun=500,  seed=9527)
+res_data_best <- NMF::nmf(data_t, k.best,nrun=1000,  seed=777)
 basismap(res_data_best, main = paste('Best NMF is ',k.best,' of',title_prefix))
 curr_plot <- recordPlot()
 pdf(paste0(title_prefix, "_", data_name, "_best_NMF.pdf"))
